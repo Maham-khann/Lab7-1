be620514291360c7174ace3fe9477f6272e49aa2
Maham Akram <107459048+Maham-khann@users.noreply.github.com>
2022-06-18 20:09:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+//se20-083 
+public class Main 
+{
+ public static < E > void printArray( E[] inputArray ) 
+  {
+ // Display array elements
+ for(E element : inputArray) {
+ System.out.printf("%s ", element);
+ }
+ System.out.println(); 
+   for (int i=inputArray.length-1; i>=0; i--)
+     System.out.print(inputArray[i]+" ");
+   } 
+public static void main(String args[]) { 
+ // Create arrays of Integer, Double and Character
+  Integer[] intArray = { 1, 2, 3, 4, 5 };
+  Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+  Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+ System.out.println("Array integerArray contains:");
+ printArray(intArray); // pass an Integer array
+ System.out.println("\nArray doubleArray contains:");
+ printArray(doubleArray); // pass a Double array
+ System.out.println("\nArray characterArray contains:");
+ printArray(charArray); // pass a Character array
+ }
+}
